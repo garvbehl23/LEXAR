ebb5cab0b9385abc522ada832633358adf59bd4a
garvbehl23 <behlgarv@gmail.com>
2026-02-17 18:06:26 +0530

    frontend v2

--- nyayaview/src/services/__init__.py
+++ nyayaview/src/services/__init__.py
@@ -1,1 +1,17 @@
-# This file is intentionally left blank.
+"""
+Services Module
+================
+Contains backend services for Bharat Nyaya Console.
+"""
+
+from .lexar_service import LexarService, RetrievedChunk, AnalysisResult
+from .grok_service import GrokService, GrokResponse, OpenAIService
+
+__all__ = [
+    'LexarService',
+    'RetrievedChunk', 
+    'AnalysisResult',
+    'GrokService',
+    'GrokResponse',
+    'OpenAIService',
+]
